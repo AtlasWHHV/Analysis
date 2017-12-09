0490879c5911fcddcb2147936ae9ab4e469c7120
Geneva M. Taylor <gennyt95@tev01.phys.washington.edu>
2017-12-08 21:07:05 -0800

    added scatterplots

--- Test_Graphs/testgraph.py
+++ Test_Graphs/testgraph.py
@@ -22,9 +22,27 @@ def make_graphs(X, y, img_dir):
     plt.savefig(os.path.join(img_dir, branch_name + '.png'))
     plt.close()
 
+def make_scatter(X, y, img_dir, val, title):
+  # plot selected branches
+  if not os.path.exists(img_dir):
+    os.mkdir(img_dir)
+  
+  # making the scatterplots
+  # note that y == 0 denotes quark data and y == 1 denotes gluon data  
+  plt.scatter(X.loc[y == val, 'ntracks'], X.loc[y == val, 'jetPt'], label='quarks')
+  plt.scatter(X.loc[y == val, 'ntracks'], X.loc[y == val, 'jetPt'], label='gluons')
+  plt.title('tracks v Pt ' + title)
+  plt.ylabel('tracks')
+  plt.xlabel('Pt')
+  plt.savefig(os.path.join(img_dir, 'scatterplot' + title +'.png'))
+  plt.close()
+
 def main():
   X, y = data.get_data()
   make_graphs(X, y, 'images')
+
+  make_scatter(X, y, 'scatter_images', 0, 'quarks')
+  make_scatter(X, y, 'scatter_images', 1, 'gluons')
 
 if __name__ == '__main__':
   main()
