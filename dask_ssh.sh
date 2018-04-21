efd0ab7a752fd91684b14b9de687a7f985886d1c
Alex Schuy <alexjschuy@gmail.com>
2018-04-21 16:09:26 -0700

    Changes to using python 3.4 to avoid 2.7 conflicts with pipenv.

--- dask_ssh.sh
+++ dask_ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DASK_SSH="$(pipenv --venv)/lib/python2.7/site-packages/distributed/cli/dask_ssh.py"
+DASK_SSH="$(pipenv --venv)/lib/python3.4/site-packages/distributed/cli/dask_ssh.py"
 pipenv run python $DASK_SSH --scheduler tev01 --hostfile hostfile.txt
 
